5b102ee4818b2323a8f45494edc84625f3f19d11
Chris Whitehouse <chris_whitehouse@hotmail.com>
2021-02-16 14:16:28 +0000

    implement, id, title, author, isbn, postcode, phoneNumber in book object whithin BookApp class

--- backend/app.js
+++ backend/app.js
@@ -2,16 +2,26 @@
 
 class BookApp {
   constructor() {
-    this.bookshelf = []
+    this.bookshelf = [],
+    this.id_counter = 1
   }
 
   getBookshelf() {
     return this.bookshelf
   }
 
-  addBook(title) {
-    this.bookshelf.push(title)
-    return title
+  addBook(title, author, isbn, postcode, phoneNumber) {
+    let book = {
+      id: this.id_counter,
+      title: title,
+      author: author,
+      isbn: isbn,
+      postcode: postcode,
+      phoneNumber: phoneNumber
+    }
+    this.bookshelf.push(book)
+    this.id_counter ++
+    return book
   }
 }
 
--- backend/app.test.js
+++ backend/app.test.js
@@ -12,13 +12,31 @@ describe("app", function() {
   });
 
   describe("#addBook", function() {
+    beforeEach( () => {
+      testApp.addBook('Just So Stories', 'Rudyard Kipling', 9780192822765, 'test_postcode', 'test_phoneNumber')
+    })
+
     it('adds a book to bookshelf with a title', function() {
-      testApp.addBook('Just So Stories')
       expect(testApp.getBookshelf()[0].title).to.equal('Just So Stories')
     });
 
+    it('adds a book to bookshelf with a author', function() {
+      expect(testApp.getBookshelf()[0].author).to.equal('Rudyard Kipling')
+    });
+
+    it('adds a book to bookshelf with a ISBN', function() {
+      expect(testApp.getBookshelf()[0].isbn).to.equal(9780192822765)
+    });
+
+    it('adds a book to bookshelf with a postcode', function() {
+      expect(testApp.getBookshelf()[0].postcode).to.equal('test_postcode')
+    });
+
+    it('adds a book to bookshelf with a phoneNumber', function() {
+      expect(testApp.getBookshelf()[0].phoneNumber).to.equal('test_phoneNumber')
+    });
+
     it('adds a book to bookshelf with an id', function() {
-      testApp.addBook('Just So Stories')
       expect(testApp.getBookshelf()[0].id).to.equal(1)
     });
   });
