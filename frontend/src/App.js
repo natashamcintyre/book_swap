1e05a979ba4ec56837ad946b0c34d2a0108e9541
Chris Whitehouse <chris_whitehouse@hotmail.com>
2021-02-16 11:11:49 +0000

    Add author text input box

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ function MyApp() {
   return (
     <div className="App">
       <input type="text" name="title" id="title" />
+      <input type="text" name="title" id="author" />
       <button type="submit" name="submit" id="submit">Submit</button>
     </div>
   );
