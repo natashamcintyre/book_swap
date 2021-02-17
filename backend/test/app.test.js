4f350be8936bccc5404332f4f6c463eab289c074
Will Dixon <dixon.will1@gmail.com>
2021-02-17 15:11:40 +0000

    errors occuring separating module and app into controller and routes files

--- backend/test/app.test.js
+++ backend/test/app.test.js
@@ -1,10 +1,10 @@
 import request from "supertest";
 import {expect} from "chai";
-import BookApp from "../app.js"
+import BookApp from "../lib/model.js"
 
 describe("Books API endpoint tests", function() {
   it.only("gets from backend bookshelf", function(done){
-    const res = request(BookApp)
+    const res = request(new BookApp)
     .get("/")
     res.expect([ {id: 1, title: 'Just So Stories', author: 'Rudyard Kipling', isbn: 9780192822765, postcode: 'test_postcode', phoneNumber: 'test_phoneNumber', availability: true } ])
     res.expect(200)
