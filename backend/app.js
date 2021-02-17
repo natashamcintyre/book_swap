a95ec98b2e83d170114afe816f3dd97c616d13f3
Natasha McIntyre <natashamc.cs@gmail.com>
2021-02-17 15:31:43 +0000

    separated into mvc components

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,11 @@
 import express from 'express'
 const app = express();
-
+const routes = require('./lib/routes.js')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
 
-app.use(routes);
+app.use('/', routes);
 
 const server = app.listen(3001, function (){
   console.log("Connected");
--- backend/test/app.test.js
+++ backend/test/app.test.js
@@ -1,11 +1,11 @@
 import request from "supertest";
 import {expect} from "chai";
-import server from "../app.js"
+import app from "../app.js"
 
 
 describe("Books API endpoint tests", function() {
   it.only("gets from backend bookshelf", function(done){
-    const res = request(server)
+    const res = request(app)
     .get("/")
     res.expect([ {id: 1, title: 'Just So Stories', author: 'Rudyard Kipling', isbn: 9780192822765, postcode: 'test_postcode', phoneNumber: 'test_phoneNumber', availability: true } ])
     res.expect(200)
