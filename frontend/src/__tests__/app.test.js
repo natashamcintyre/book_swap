fbe3df705979a3f39842e14001999c0cc952f0da
Chris Whitehouse <chris_whitehouse@hotmail.com>
2021-02-17 17:33:34 +0000

    Add failing BookMeUp error handling tests

--- frontend/src/__tests__/app.test.js
+++ frontend/src/__tests__/app.test.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import BookMeUp from '../App';
 
 import mockAxios from '../__mocks__/axios.js'
+import errorMock from '../__mocks__/error.json'
 import Enzyme from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import { mount } from 'enzyme'
@@ -11,14 +12,16 @@ Enzyme.configure({ adapter: new Adapter() });
 
 describe('BookMeUp', () => {
   beforeEach(() => {
-    mockAxios.get.mockImplementation(() =>
-    Promise.resolve({ data: [] }));
     mockAxios.post.mockImplementation(() =>
+    Promise.resolve({ data: [] }));
+
+    mockAxios.get.mockImplementation(() =>
     Promise.resolve({ data: [] }));
   });
 
   afterEach(() => {
     mockAxios.post.mockClear()
+    mockAxios.get.mockClear()
   })
 
   it('Loads data from api', () => {
@@ -55,3 +58,48 @@ describe('BookMeUp', () => {
     expect(component.instance().refs.bookFormRef.state.title).toEqual('');
   })
 });
+
+describe('BookMeUp erroring', () => {
+  beforeEach(function(){
+    mockAxios.post.mockImplementation(() =>
+    Promise.reject(errorMock))
+
+    mockAxios.get.mockImplementation(() =>
+    Promise.reject(errorMock))
+  })
+
+  afterEach(function(){
+    mockAxios.post.mockClear()
+    mockAxios.get.mockClear()
+  })
+
+  it('loads err on GET err', async () => {
+    const component = await mount(<BookMeUp />);
+
+    await component.update()
+    expect(mockAxios.get).toHaveBeenCalledTimes(1);
+    expect(component.state().error).toEqual({"response": {"data": "error text from json mock"}});
+    expect(component.find('#error').text()).toBe('Error: error text from json mock');
+  });
+
+  it('loads err on Post err', async () => {
+    const component = mount(<BookMeUp/>);
+
+    component.find('input#title').simulate('change', {
+      target: { value: "" } })
+    component.find('input#author').simulate('change', {
+      target: { value: "" } })
+    component.find('input#ISBN').simulate('change', {
+      target: { value: "" } })
+    component.find('input#postcode').simulate('change', {
+      target: { value: "" } })
+    component.find('input#phone_number').simulate('change', {
+      target: { value: "" } })
+
+    await componenet.find('form').simulate('submit')
+    await component.update()
+    expect(mockAxios.post).toHaveBeenCalledTimes(1)
+    expect(component.state().error).toEqual({"response": {"data": "error text from json mock"}});
+    expect(component.find('#error').text()).toBe('Error: error text from json mock');
+  })
+})
