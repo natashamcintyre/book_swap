99376d436931d6dc38c136857e1c9a4a8f6395d7
Chris Whitehouse <chris_whitehouse@hotmail.com>
2021-02-18 19:03:18 +0000

    tidy up files

--- backend/lib/controller.js
+++ backend/lib/controller.js
@@ -1,31 +1,13 @@
-
-// import BookApp from './model.js'
 import BookModel from './model'
 
 function getBookshelf() {
   return BookModel.find()
-  // return new Promise((resolve, reject) => {
-  //   var result = bookApp.getBookshelf()
-  //   if (result !== []) {
-  //     resolve(result)
-  //   } else {
-  //     reject(result)
-  //   }
-  // })
+
 }
 
 function addBook(data) {
   let newBook = new BookModel({data: data})
   return newBook.save()
-  // return new Promise((resolve, reject) => {
-  //   console.log(`in promise`)
-  //   var result = bookApp.addBook(data)
-  //   if (result !== []) {
-  //     resolve(result)
-  //   } else {
-  //     reject(result)
-  //   }
-  // })
 }
 
 module.exports = {
--- backend/lib/model.js
+++ backend/lib/model.js
@@ -1,6 +1,6 @@
 'use strict'
 
-import mongoose from 'mongoose'; 
+import mongoose from 'mongoose';
 
 let BookSchema = mongoose.Schema({
   data: Object
@@ -10,105 +10,16 @@ BookSchema.post('validate', function(doc, next) {
   if (!doc.data.title || !doc.data.author || !doc.data.isbn || !doc.data.postcode || !doc.data.phoneNumber) {
     next('Invalid book entry')
   };
-  next() 
+  next()
 })
 
 BookSchema.post('find', function(doc, next){
   if (doc.length === 0){
     next('No books in database')
   };
-  next() 
+  next()
 })
 
 let BookModel = mongoose.model('books',BookSchema);
 
 export default BookModel; 
-// import fs from 'fs'
-// import path from 'path'
-
-// function newID(array){
-//   if (array.length > 0) {
-//     return array[array.length-1].id + 1;
-//   } else {
-//     return 1
-//   }
-// }
-
-
-// class BookApp {
-//   constructor (filepath) {
-//     // this.id_counter = 1
-//     this.filepath = filepath
-//     this.bookshelf = filepath ? this.readFromJson() : []
-//   }
-
-//   addBook(data) {
-//     console.log(`in model addBook`)
-//     if (data.title && data.author && data.isbn && data.postcode && data.phoneNumber) {
-//       const book = {
-//         id: newID(this.bookshelf),
-//         title: data.title,
-//         author: data.author,
-//         isbn: data.isbn,
-//         postcode: data.postcode,
-//         phoneNumber: data.phoneNumber,
-//         availability: true
-//       }
-//       this.bookshelf.push(book)
-//       this.writeToJson()
-//       return this.bookshelf
-//     } else {
-//       throw 'Invalid book entry'
-//     }
-//   }
-
-//   getBookshelf () {
-//     return this.bookshelf
-//   }
-
-//   getBookById (id) {
-//     return this.bookshelf.filter(book => book.id === id)[0]
-//   }
-
-//   updateAvailabilityById (id) {
-//     let index = this.bookshelf.findIndex(book => book.id === id)
-//     if (index >= 0) {
-//       this.bookshelf[index].availability = this.bookshelf[index].availability !== true
-//       this.writeToJson()
-//       return this.bookshelf
-//     } else {
-//       throw 'Book not found on bookshelf'
-//     }
-//   }
-
-//   deleteBookById (id) {
-//     let index = this.bookshelf.findIndex(book => book.id === id)
-//     if (index >= 0) {
-//       this.bookshelf = this.bookshelf.filter(book => book.id !== id)
-//       this.writeToJson()
-//       return this.bookshelf
-//     } else {
-//       throw 'Book not found on bookshelf'
-//     }
-//   }
-
-//   writeToJson () {
-//     if (this.filepath) {
-//       const jsonItem = JSON.stringify(this.bookshelf)
-
-//       fs.writeFileSync(__dirname + path.normalize(this.filepath), jsonItem, (err) => {
-//         if (err) throw err
-//       })
-//     }
-//   }
-
-//   readFromJson () {
-//     return JSON.parse(fs.readFileSync(
-//       __dirname + path.normalize(this.filepath), 'utf8', (err, data) => {
-//         if (err) throw err
-//       })
-//     )
-//   }
-// }
-
-// export default BookApp
