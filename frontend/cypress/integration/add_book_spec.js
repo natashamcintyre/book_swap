503a277e2c04c3e59fa0c535cec83afed266a0ac
Natasha McIntyre <natashamc.cs@gmail.com>
2021-02-18 17:38:46 +0000

    add the add book test

--- frontend/cypress/integration/add_book_spec.js
+++ frontend/cypress/integration/add_book_spec.js
@@ -1,5 +1,6 @@
-describe('Our First Test', () => {
-  it('Does not do much!', () => {
-    expect(true).to.equal(true)
+describe('user can add a book', () => {
+  it('displays new book on page', () => {
+    cy.visit('http://localhost:3000')
+    cy.get('form').should('exist')
   })
 })
