77fd0244c347642761b58a101519e2ff15c2ae2e
Kiki Dawson <kikiadawson@gmail.com>
2021-02-19 18:28:09 +0000

    Fix error

--- backend/lib/routes.js
+++ backend/lib/routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express"
-const bookApp = require('./controller.js')
+const bookApp = require('./controller.js');
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const auth = require("../middleware/auth");
-const User = require("../models/userModel");
-const router = Router()
+const User = require("./userModel");
+const router = Router();
 
 router.get('/', async (req, res) => {
   await bookApp.getBookshelf()
@@ -20,29 +20,38 @@ router.post('/add-book', async (req, res) => {
 
 router.post('/register', async (req, res) => {
   try {
-    let { username, email, password, passwordCheck, location } = req.body; 
+    let { username, email, password, passwordCheck, location } = req.body;
 
     if(!username || !email || !password || !passwordCheck || !location)
       return res.status(400).json({ msg: "Not all fields have been filled"});
     if(password.length < 6)
       return res
         .status(400)
-        .json({ msg: "Password needs to be 6 characters or longer"}); 
+        .json({ msg: "Password needs to be 6 characters or longer"});
     if(password !== passwordCheck)
       return res
         .status(400)
         .json({ msg: "Enter the password twice to be verified"});
 
-      
     const existingUser = await User.findOne({  username: username});
     const existingEmail = await User.findOne({ email: email});
+    if (existingUser || existingEmail)
+      return res
+        .status(400)
+        .json({ msg: "An account with this username and email exists"})
 
-      if (existingUser || existingEmail)
-        return res
-          .status(400)
-          .json({ msg: "An account with this username and email exists"})
+    const newUser = new User({
+      username,
+      email,
+      location,
+      password,
+    });
 
-    
+    const savedUser = await newUser.save();
+    re.json(savedUser);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 
