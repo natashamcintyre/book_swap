a57209313b9d202f42c96f0f9cf606f441b4167b
Aman Tank <amantank187@gmail.com>
2021-02-19 16:47:08 +0000

    Added a register route and logic

--- backend/lib/routes.js
+++ backend/lib/routes.js
@@ -1,5 +1,7 @@
 import { Router } from "express"
 const bookApp = require('./controller.js')
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const auth = require("../middleware/auth");
 const User = require("../models/userModel");
 const router = Router()
@@ -15,6 +17,34 @@ router.post('/add-book', async (req, res) => {
   .then((book) => res.json(book))
   .catch((err) => res.status(404).json(err))
 });
+
+router.post('/register', async (req, res) => {
+  try {
+    let { username, email, password, passwordCheck, location } = req.body; 
+
+    if(!username || !email || !password || !passwordCheck || !location)
+      return res.status(400).json({ msg: "Not all fields have been filled"});
+    if(password.length < 6)
+      return res
+        .status(400)
+        .json({ msg: "Password needs to be 6 characters or longer"}); 
+    if(password !== passwordCheck)
+      return res
+        .status(400)
+        .json({ msg: "Enter the password twice to be verified"});
+
+      
+    const existingUser = await User.findOne({  username: username});
+    const existingEmail = await User.findOne({ email: email});
+
+      if (existingUser || existingEmail)
+        return res
+          .status(400)
+          .json({ msg: "An account with this username and email exists"})
+
+    
+});
+
 
 router.get('/user', auth, async (req, res) => {
   const user = await User.findById(req.user);
