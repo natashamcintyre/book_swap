755d5e177ce2dc879a56741e3742e98dc491646b
Natasha McIntyre <natashamc.cs@gmail.com>
2021-02-20 16:38:47 +0000

    add sign up form

--- frontend/src/components/users.js
+++ frontend/src/components/users.js
@@ -1,9 +1,67 @@
 import React from 'react'
 
 class Users extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      username: '',
+      email: '',
+      location: '',
+      password: '',
+      passwordCheck: ''
+    }
+  }
+
+  changeUsernameValue(username) {
+    this.setState({
+      username: username
+    })
+  }
+
+  changeEmailValue(email) {
+    this.setState({
+      email: email
+    })
+  }
+
+  changeLocationValue(location) {
+    this.setState({
+      location: location
+    })
+  }
+
+  changePasswordValue(password) {
+    this.setState({
+      password: password
+    })
+  }
+
+  changePasswordCheckValue(password) {
+    this.setState({
+      passwordCheck: password
+    })
+  }
+
+  processSubmit(e) {
+    e.preventDefault()
+    this.addUser(this.state.username, this.state.email, this.state.location, this.state.password, this.state.passwordCheck)
+    // post user to back end.
+    // if save is successful, navigate to homepage
+    // else display error and stay on this page. Highlight error area???
+  }
+
   render() {
     return(
-      <p> Users component </p>
+      <div className="new_user">
+        <form id="new_user_form" onSubmit={ (e) => this.processSubmit(e) }>
+          <input type="text" name="username" id="new_username" placeholder="Choose a Username" onChange={(e) => this.changeUsernameValue(e.target.value)} value={this.state.username} />
+          <input type="text" name="email" id="new_email" placeholder="Enter your email address" onChange={(e) => this.changeEmailValue(e.target.value)} value={this.state.email} />
+          <input type="text" name="location" id="new_location" placeholder="Enter your postcode" onChange={(e) => this.changeLocationValue(e.target.value)} value={this.state.location} />
+          <input type="text" name="password" id="new_password" placeholder="Type your password" onChange={(e) => this.changePasswordValue(e.target.value)} value={this.state.password} />
+          <input type="text" name="passwordCheck" id="new_passwordCheck" placeholder="Type your password again" onChange={(e) => this.changePasswordCheckValue(e.target.value)} value={this.state.passwordCheck} />
+          <button type="submit" name="submit" id="submit">Sign Me Up</button>
+        </form>
+      </div>
     )
   }
 }
