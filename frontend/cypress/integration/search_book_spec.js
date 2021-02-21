697ed5130796472257d2e127f08e2f7407f927bc
Chris Whitehouse <chris_whitehouse@hotmail.com>
2021-02-21 14:01:56 +0000

    Add passing BookSearch unit tests

--- frontend/cypress/integration/search_book_spec.js
+++ frontend/cypress/integration/search_book_spec.js
@@ -2,7 +2,7 @@ describe('user search for a book with ISBN', () => {
   it('auto-populates add_book_form', () => {
     cy.visit('http://localhost:3000')
     cy.get('form[id="book_search"]').should('exist')
-    cy.get('input[name="ISBNSearch"]').type(9780099590088).should("have.value", 9780099590088)
+    cy.get('input[id="ISBNSearch"]').type(9780099590088).should("have.value", 9780099590088)
     cy.get('form[id="book_search"]').submit()
     cy.get('input[name="title"]').should("have.value", "Sapiens")
     cy.get('input[name="author"]').should("have.value", "Yuval Noah Harari")
