0e090d99b1d14818155f577c3cb8b3c8f1c3d22a
Kiki Dawson <kikiadawson@gmail.com>
2021-02-21 13:58:25 +0000

    Change icon in navbar and add one to header

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,11 +1,11 @@
 import React from 'react'
-// import { Container } from 'react-grid-system'
 
 class Header extends React.Component {
   render () {
     return (
       <div className="header col-12">
         <div className="header-content col-12 col-md-6 offset-md-3">
+          <img src="images/logo.png"></img>
           <h1>Welcome to Book Me Up!</h1>
           <p>Books will be place in here in the near future</p>
           <button className="btn btn-outline-secondary btn-lg">How to get a book!</button>
