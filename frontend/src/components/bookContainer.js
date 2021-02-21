d68d8adb30cda1f9c257573258e0739f445b4713
Kiki Dawson <kikiadawson@gmail.com>
2021-02-21 17:19:26 +0000

    Correct bookComponent test - now passing

--- frontend/src/components/bookContainer.js
+++ frontend/src/components/bookContainer.js
@@ -6,7 +6,7 @@ class BookContainer extends React.Component {
   render () {
     return (
       <Col lg={2} md={4} sm={12}>
-          <div key={this.props.book._id}>
+          <div className='book-data' key={this.props.book._id}>
             {/* <p>Book ID will need deleting! {this.props.book._id}</p> */}
             <div className="book-title">
               {this.props.book.data.title}
