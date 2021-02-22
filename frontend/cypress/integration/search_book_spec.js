96f010110b7b694aa49c16b4192dcb5859a886d6
Kiki Dawson <kikiadawson@gmail.com>
2021-02-22 15:06:04 +0000

    Adding feature tests

--- frontend/cypress/integration/search_book_spec.js
+++ frontend/cypress/integration/search_book_spec.js
@@ -1,11 +1,16 @@
 describe('user search for a book with ISBN', () => {
   it('auto-populates add_book_form', () => {
     cy.visit('http://localhost:3000')
+    cy.get('.btn#addBook').click()
     cy.get('form[id="book_search"]').should('exist')
     cy.get('input[id="ISBNSearch"]').type('9780739360385').should('have.value', '9780739360385')
     cy.get('form[id="book_search"]').submit()
+    cy.get('form[id="add_book"]').should('exist')
     cy.get('input[id="title"]').should('have.value', 'Harry Potter and the Deathly Hallows')
     cy.get('input[id="author"]').should('have.value', 'J. K. Rowling')
     cy.get('input[id="ISBN"]').should('have.value', '9780739360385')
+    cy.get('form[id="add_book"]').submit()
+    cy.get('div[id="books_list"]').should('contain', 'Harry Potter and the Deathly Hallows')
+    cy.get('div[id="books_list"]').should('contain', 'J. K. Rowling')
   })
 })
