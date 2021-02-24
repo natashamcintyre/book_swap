e40a61af75507f2371f9eb7e352cce22fc4cfe1f
AmanTank187 <37899538+AmanTank187@users.noreply.github.com>
2021-02-24 11:26:26 +0000

    Removed comments

--- frontend/src/components/barcode/barcodeScanner.js
+++ frontend/src/components/barcode/barcodeScanner.js
@@ -23,12 +23,6 @@ class BarcodeScanner extends Component {
         <button onClick={this._scan}>
           {this.state.scanning ? 'Stop' : 'Start'}
         </button>
-        {/* <ul className="results">
-          {this.state.results.map((result, i) => (
-            <Result key={result.codeResult.code + i} result={result} />
-          ))}
-        </ul> */}
-
         {this.state.scanning ? <Scanner onDetected={this._onDetected} /> : null}
       </div>
     )
