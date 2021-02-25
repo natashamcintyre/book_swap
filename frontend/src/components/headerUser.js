ba13e1ea3987eecf2869389c0082d91eda12ccc6
Natasha McIntyre <44112878+natashamcintyre@users.noreply.github.com>
2021-02-25 17:22:48 +0000

    Update headerUser.js

--- frontend/src/components/headerUser.js
+++ frontend/src/components/headerUser.js
@@ -3,21 +3,6 @@ import PropTypes from 'prop-types'
 import UserSignin from './userSignin.js'
 
 class HeaderUser extends React.Component {
-  constructor () {
-    super()
-    this.state = { show: false }
-    this.showModal = this.showModal.bind(this)
-    this.hideModal = this.hideModal.bind(this)
-  }
-
-  showModal = () => {
-    this.setState({ show: true })
-  }
-
-  hideModal = () => {
-    this.setState({ show: false })
-  }
-
   render () {
     return (
       <div className="header col-12">
